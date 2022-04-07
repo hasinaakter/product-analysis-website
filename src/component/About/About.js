4194daf3a0663ae4f6e5106c917946817e00ce5a
Hasina Akter <hasina2421@gmail.com>
2022-04-07 13:59:54 +0600

    add about details

--- src/component/About/About.js
+++ src/component/About/About.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React from "react";
 
 const About = () => {
-    return (
-        <div>
-            <h2>About</h2>
+  return (
+    <div>
+      <h1>About LENOVO ideapad 320 </h1>
+
+      <div className="home-container">
+        <div className="laptop-details">
+          <h3>Powerful hybrid storage</h3>
+          <p>
+            Choose between the ample storage of up to 2 TB SATA HDD or
+            performance-enhancing hybrid storage, with up to 128 GB PCIe SSD
+            plus 1 TB SATA HDD. You'll have more than enough room to build your
+            collection of photos, movies and more — without spending extra
+            money.
+          </p>
         </div>
-    );
+        <div className="laptop-images">
+          <img src="images/lenovo-ideapad-d.jpg" alt="" />
+        </div>
+      </div>
+      <div className="home-container">
+        <div className="laptop-images">
+          <img src="images/lenovo-ideapad-320-15-G.jpg" alt="" />
+        </div>
+        <div className="laptop-details">
+          <h3>Advanced graphics power with NVIDIA </h3>
+          <p>
+            The Ideapad 320 offers a range of graphics options, including a
+            powerful NVIDIA® GeForce™ 940MX graphics card. Discrete graphics
+            cards rely on their own processing power, so you'll experience
+            smoother graphics, less screen tearing and better gaming performance
+            without compromising overall speed and responsiveness. Whether
+            you're playing a game or creating or editing content, you'll enjoy
+            crisp visuals.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
--- src/component/NotFound/NotFound.js
+++ src/component/NotFound/NotFound.js
@@ -3,6 +3,8 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div className='error'>
+            <h3>OOOPS! Page Not Found.</h3>
+            <p>Try Again Please!</p>
             <img src="./images/404error.jpg" height='450' alt=""/>
         </div>
     );
